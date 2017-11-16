ed39066ef35363ef699cd89d5b5256c7158fefa8
Milan <milan@plugin.company>
2017-11-16 11:14:34 -0800

    remove license

--- initMage.php
+++ initMage.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Created by:  Milan Simek
- * Company:     Plugin Company
- *
- * LICENSE: http://plugin.company/docs/magento-extensions/magento-extension-license-agreement
- *
- * YOU WILL ALSO FIND A PDF COPY OF THE LICENSE IN THE DOWNLOADED ZIP FILE
- *
- * FOR QUESTIONS AND SUPPORT
- * PLEASE DON'T HESITATE TO CONTACT US AT:
- *
- * SUPPORT@PLUGIN.COMPANY
- */
 ini_set('display_errors', 1);
 require_once('../app/Mage.php');
 umask(0);
